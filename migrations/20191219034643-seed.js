7165684617c6dcec9b12beca2ef958f192014a0f
Do Trung Duc <duc010298@gmail.com>
2019-12-19 12:07:46 +0700

    .

--- migrations/20191219034643-seed.js
+++ migrations/20191219034643-seed.js
@@ -19,7 +19,7 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db) {
-  var filePath = path.join(__dirname, 'sqls', 'V1__Init_database.sql');
+  var filePath = path.join(__dirname, 'sqls', '20191219034643-seed.sql');
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
       if (err) return reject(err);
