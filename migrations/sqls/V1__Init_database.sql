e31c803e7b18d67da3fcad0f99e4ac8a62a128d9
Do Trung Duc <duc010298@gmail.com>
2019-12-19 11:10:02 +0700

    test

---
+++ migrations/sqls/V1__Init_database.sql
@@ -0,0 +1,9 @@
+create table if not exists server_ip
+(
+    id int primary key,
+    ip varchar(15) not null,
+    lastUpdate timestamp not null
+);
+
+insert into server_ip (id, ip, lastUpdate)
+values (1, '127.0.0.1', NOW());
---
+++ src/models/server-ip.ts
@@ -0,0 +1,5 @@
+export interface ServerIP {
+    id: string;
+    ip: string;
+    lastUpdate: Date;
+}
