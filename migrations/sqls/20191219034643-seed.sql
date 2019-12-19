ba5193e8a15ed3c6061ee550360abb81be94e6fd
Do Trung Duc <duc010298@gmail.com>
2019-12-19 18:24:12 +0700

    .

--- migrations/sqls/20191219034643-seed.sql
+++ migrations/sqls/20191219034643-seed.sql
@@ -2,10 +2,9 @@ set time zone +7;
 
 create table if not exists server_ip
 (
-    id int primary key,
     ip varchar(15) not null,
     lastUpdate timestamp not null
 );
 
-insert into server_ip (id, ip, lastUpdate)
-values (1, '127.0.0.1', NOW());
+insert into server_ip (ip, lastUpdate)
+values ('127.0.0.1', NOW());
