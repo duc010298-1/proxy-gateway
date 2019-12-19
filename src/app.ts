968e7c89385ff32b678f31c6561e5ef359fc3d92
Do Trung Duc <duc010298@gmail.com>
2019-12-19 18:31:17 +0700

    add view engine

--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,12 @@ const app: Application = express();
 const dbServices = new DBServices();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.get('/server-ip', (req: Request, res: Response) => {
     dbServices.getIp().then(
         (val: any) => {
-            const html = renderHtml(val);
-            res.writeHead(200, {
-                'Content-Type': 'text/html',
-                // tslint:disable-next-line: object-literal-sort-keys
-                'Content-Length': html.length,
-                'Expires': new Date().toUTCString(),
-            });
-            res.end(html);
+            res.render('viewIp', { title: 'Hey', message: val });
         },
     );
 });
@@ -29,8 +23,3 @@ app.post('/server-ip', (req: Request, res: Response) => {
 });
 
 app.listen(process.env.PORT || Config.serverPort, () => console.log('Server running...'));
-
-const renderHtml = (valueIp: any): string => {
-    const originHtml = '<!DOCTYPE html><html><head> <title>Server IP</title></head><body> <h1 style="text-align: center; margin: 100px auto 0 auto">$value</h1></body></html>';
-    return originHtml.replace('$value', valueIp);
-};
