6b2f4d1ab5ae3d09e8ca02c7d814b3eae3be1f96
Đỗ Trung Đức <duc010298@gmail.com>
2022-02-19 09:35:34 +0700

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,6 @@ export enum Config {
     pgHost = 'localhost',
     pgPort = '5432',
     pgUsername = 'postgres',
-    pgPassword = 'fsoft@12345',
+    pgPassword = '12345678',
     pgDatabase = 'postgres',
 }
